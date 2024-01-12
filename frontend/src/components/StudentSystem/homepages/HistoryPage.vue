a9b66c32b3b57e55cf0f51baadf748b55f0bdcfd
BoringMarsh <279623342@qq.com>
2024-01-12 20:41:29 +0800

    修复浏览记录查询

--- frontend/src/components/StudentSystem/homepages/HistoryPage.vue
+++ frontend/src/components/StudentSystem/homepages/HistoryPage.vue
@@ -19,13 +19,13 @@
             </el-col>
         </el-row>
         <!-- 列表区域 -->
-        <el-table :data="tableData" style="width: 100%">
+        <el-table :data="browseData" style="width: 100%">
             <el-table-column prop="actId" label="活动ID" width="120"></el-table-column>
             <el-table-column prop="broTimeStart" label="浏览时间" width="180"></el-table-column>
             <el-table-column prop="browserId" label="浏览者ID" width="120"></el-table-column>
             <el-table-column prop="whetherBuy" label="是否购买" width="120">
                 <template v-slot="scope">
-                    {{ scope.row.whetherBuy ? '是' : '否' }}
+                    {{ scope.row.whetherBuy == 0 ? '是' : '否' }}
                 </template>
             </el-table-column>
         </el-table>
@@ -36,7 +36,6 @@
 <script setup lang="ts">
 import {reactive,onActivated,onMounted, ref} from 'vue';
 import {useRouter} from 'vue-router'
-import  baseURL  from "../../../../router/baseURL.js";
 import {store}from '../../../../router/store'
 import axios from 'axios';
 import {
@@ -46,33 +45,30 @@ import {
 import { ElMessageBox} from 'element-plus';
 
 const user_id=ref();
-
-let tableData = reactive([    {
+const browseData = ref([
+    {
         "broTimeStart": "2021-05-18 16:56:46",
         "actId": 1,
         "browserId": 1,
-        "whetherBuy": false
-    },
-    {
-        "broTimeStart": "2022-05-18 16:56:46",
-        "actId": 2,
-        "browserId": 1,
-        "whetherBuy": false
-    }])
+        "whetherBuy": 0
+    }
+])
+
+
 async function getData(){
     user_id.value=sessionStorage.getItem("stuId")
 
-    await axios.get(baseURL + `/api/activity-personal/browse/1`)
+    await axios.get('http://localhost:8084/api/activity-personal/browse?userId='+user_id.value)
     .then(response=>{
         console.log(response.data)
-        tableData = response.data
+        browseData.value = JSON.parse(JSON.stringify(response.data));
     })
 }
 
 //显示的记录
 onMounted(()=>{
     user_id.value=sessionStorage.getItem("stuId")
-    // getData()
+    getData()
     //记得改回来
     //user_id.value=1000000
 })
